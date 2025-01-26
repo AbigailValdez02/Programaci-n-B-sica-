34c0ae2556b5605326474eb1eee27b428910ee29
AbigailValdez02 <valdezgarciaabigail@gmail.com>
2025-01-25 21:50:52 -0600

    Create folder "Semana 1"

--- Ejercicios Cap1.py
+++ Ejercicios Cap1.py
@@ -1,2 +1,39 @@
-#Ejercicio1: ¿Cual es la funcion de la memoria secundaria en una computadora?
-Respuesta: a
+#Ejercicio 1: ¿Cual es la funcion de la memoria secundaria en una computadora?
+Respuesta: c)Almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado  y encendido. 
+
+#Ejercicio 2: ¿Que es un programa?
+Respuesta: Un conjunto de instrucciones que indican como realizar algun tipo de calculo. 
+
+ #Ejercicio 3: ¿Cual es la diferencia entre un compilador y un interprete?
+Respuesta: Que el interprete traduce linea a linea sobre la marcha y el compilador necesita que le entreguen el programa completo. 
+
+#Ejercicio 4: ¿Cual de los siguientes contiene "codigo maquina"?
+Respuesta: a) El interprete de Python. 
+
+#Ejercicio 5: ¿Que esta mal en el siguiente codigo?
+>>> primt "¡Hola, mundo!"
+file "<stdin>", line 1
+primt "¡Hola, mundo!"
+Respuesta: Escribieron "primt" en lugar de "print"
+
+#Ejercicio 6: ¿En que lugar del computador queda almacenada una variable, como en este caso "x", despues de ejecutar la siguiente linea de Python?: x = 123
+Respuesta:b) Memoria principal. 
+
+#Ejercicio 7: ¿Que mostrara en pantalla el siguiente programa?:
+x = 43 
+x = x + 1
+print (x)
+Respuesta: b) 44
+
+#Ejercicio 8: Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+-Unidad central de el procesamiento: Es el componente de la computdora diseñadopara ejecutar las tareas necesarias, como  el cerebro humano.
+-Memoria principal:Se usa para almacenar informacion que la CPU necesita de forma inmediata, como por ejemplo la memoria a corto plazo. 
+-Memoria secundaria:Almacena informacion incluso cuando se interrumpee la alimentacion, es usualmente mas lenta que la memoria principal; como la memoria a largo plazo.
+-Dispositivos de entrada:dispositivos que facilitan introducir informacion a la computadora, como el raton y el teclado, actuan como los sentidos humanos.
+-Dispositivos de salida: Se utilizan para mostrar resultados, como los humanos y su manera de comunicarse.  
+
+por ejemplo "¿Cual seria el equivalente humano de la Unidad central del procesamiento?"
+Respuesta: El cerebro. 
+
+#Ejercicio 9: ¿Como puedes corregir un "Error de sintaxis"?
+Respuesta: revisando cuidadosamente las lineas asegurandose de cumplir con las lenguas de el lenguaje. 
