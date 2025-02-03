2f9a2ff6ae4ec291b2762ade94911b98029d880f
AbigailValdez02 <valdezgarciaabigail@gmail.com>
2025-02-03 16:43:23 -0600

    create foler "100 problemas"

---
+++ 100 problemas/problema4.py
@@ -0,0 +1,12 @@
+n = int(input("Ingrese el numero de terminos:"))
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
+    fib_secuencia = [0, 1]
+    for n in range (2, n):
+     fib_secuencia.append(fib_secuencia[-1] + fib_secuencia[-2])
+    return fib_secuencia
+print(fibonacci(n))
---
+++ 100 problemas/problema5.py
@@ -0,0 +1,15 @@
+n = int(input("Ingrese un numero:"))
+
+if n <= 1:
+    print("El numero no es primo")
+elif n == 2:
+    print("El numero es primo")
+elif n > 2:
+    if n % 2 == 0:
+        print("El numero no es primo")
+    elif n == 3:
+        print("El numero es primo")
+    elif n % 3 == 0:
+        print("El numero no es primo")
+    else:
+        print("El numero es primo")
