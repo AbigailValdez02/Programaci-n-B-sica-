431e626545f17a72293e7214503a4ef3ea10bb00
AbigailValdez02 <valdezgarciaabigail@gmail.com>
2025-02-22 23:30:56 -0600

    100 problems

--- 100 problemas/problema 21-30/problema23.py
+++ 100 problemas/problema 21-30/problema23.py
@@ -1,1 +1,30 @@
 #IMPLEMENTAR Y OPERAR CON MATRICES  
+
+A = [[1, 2, 7, 5],
+     [8, 3, 0, 2],
+     [3, 2, 9, 6]]
+
+B = [[7, 2, 8, 0],
+     [1, 0, 3, 8],
+     [2, 6, 3, 4]]
+
+def sumar_matrices(m1, m2):
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+        m3 = []
+        for i in range(len(m1)):
+            m3.append([])
+            for j in range(len(m1[0])):
+                m3[i].append(m1[i][j] + m2[i][j])
+        return m3
+    else:
+        return None
+    
+c = sumar_matrices(A, B)
+if c == None:
+    print("No se pueden sumar")
+else:
+    for fila in c:
+        print("[", end=" ")
+        for elemento in fila:
+            print(elemento, end= " ")
+        print("]")
--- 100 problemas/problema 21-30/problema27.py
+++ 100 problemas/problema 21-30/problema27.py
@@ -1,1 +1,7 @@
 #CREAR UN CONVERSOR DE UNIDADES
+
+op_medidas = int(input("***MENU PRINCIPAL***\n1. MEDIDAS DE LONGITUD\n2. MEDIDAS DE VOLUMEN\n3. MEDIDAS DE PESO\nEscoja la opcion deseada"))
+if op_medidas == 1:
+    print("***MEDIDAS DE LONGITUD***")
+    op_long = int(input("Escoja una opcion\n1. Pulgadas a milimetros\n2. Yardas a metros"))
+    
